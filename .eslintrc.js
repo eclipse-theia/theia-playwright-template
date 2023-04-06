6d999c80615ba89b6595eb1916ff39c74958b769
Nina Doschek <ndoschek@eclipsesource.com>
2023-04-06 14:51:16 +0200

    Update Playwright framework and @theia/playwright
    - Update Theia to next version
      - Update test configuration to adapt to latest version
      - Remove obsolete multiple config files and scripts
    - Update playwright to 1.32.1
    - Update allure versions
    - Add ESLint support
    - Add VS Code Playwright extension as recommended
    - Update README
    
    Signed-off-by: Nina Doschek <ndoschek@eclipsesource.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-    root: true,
     extends: [
-        './configs/base.playwright.eslintrc.json',
-        './configs/warnings.eslintrc.json',
-        './configs/errors.eslintrc.json',
-        './configs/ui-tests.eslintrc.json',
-        './configs/ui-tests.playwright.eslintrc.json'
+        './configs/base.eslintrc.json',
+        './configs/errors.eslintrc.json'
     ],
-    ignorePatterns: [
-        '**/{node_modules,lib}'
-    ],
+    ignorePatterns: ['playwright.config.ts'],
     parserOptions: {
         tsconfigRootDir: __dirname,
         project: 'tsconfig.json'
